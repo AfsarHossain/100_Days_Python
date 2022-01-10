bfbe024d35ee62b7668ceba8492da1c37f9b1a33
AfsarHossain <afsar291722@gmail.com>
2022-01-11 00:20:09 +0600

    Inter OPP

---
+++ Code/17. Inter OOP/1. Class/main.py
@@ -0,0 +1,18 @@
+class User:
+    pass
+
+
+user1 = User()
+user1.id = 1
+user1.name = "Afsar"
+
+# print(user1.id)
+# print(user1.name)
+
+print(f"{user1.id}: {user1.name}.")
+
+user2 = User()
+user2.id = 2
+user2.name = "Tarek"
+
+print(f"{user2.id}: {user2.name}.")
