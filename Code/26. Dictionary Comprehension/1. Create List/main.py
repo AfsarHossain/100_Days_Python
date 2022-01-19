bc5f572045fd210ef19c625100556ba05cad7726
AfsarHossain <afsar291722@gmail.com>
2022-01-19 12:13:06 +0600

    Dictionary Create list added

---
+++ Code/26. Dictionary Comprehension/1. Create List/main.py
@@ -0,0 +1,11 @@
+import random
+
+names = ["Alex", "Beth", "Caroline", "Dave", "Elanor", "Freddie"]
+
+# TODO: new_dict = {new_key:new_value for item in list}
+students_scores = {student: random.randint(1, 100) for student in names}
+print(students_scores)
+# TODO: new_dict = {new_key:new_value for (key, value) in dictionary.items()}
+passed_students = {student: score for (student, score) in students_scores.items() if score >= 60}
+print(passed_students)
+# TODO: new_dict = {new_key:new_value for (key, value) in dict.items() if test}
