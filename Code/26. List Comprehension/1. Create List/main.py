20ba52a7d1867a3402ee03c41061061290ff35ff
AfsarHossain <afsar291722@gmail.com>
2022-01-20 22:27:26 +0600

    List Comprehension Create list added

---
+++ Code/26. List Comprehension/1. Create List/main.py
@@ -0,0 +1,32 @@
+# TODO: 1 -> For loop
+# numbers = [1, 2, 3]
+# new_list = []
+# for n in numbers:
+#     add_1 = n + 1
+#     new_list.append(add_1)
+# print(new_list)
+
+# TODO: List Comprehension
+
+# TODO: 2
+# new_list = [new_item for item in list]
+# numbers = [1, 2, 3]
+# new_list = [n + 1 for n in numbers]
+# print(new_list)
+
+# TODO: 3 -> String as list
+# name = "Afsar"
+# letter_list = [letter for letter in name]
+# print(letter_list)
+
+# TODO: 4 -> Range list
+# range_list = [num * 2 for num in range(1, 5)]
+# print(range_list)
+
+# TODO: 5 -> Conditional List Comprehension
+# new_list = [new_item for item in list if test]
+# names = ["Alex", "Beth", "Caroline", "Dave", "Elanor", "Freddie"]
+# short_name = [name for name in names if len(name) < 5]
+# print(short_name)
+# short_name = [name.upper() for name in names if len(name) > 4]
+# print(short_name)
