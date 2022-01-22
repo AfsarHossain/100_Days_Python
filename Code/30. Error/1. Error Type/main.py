2e827fadedaa81aea3fa099a2a9b460f70ab6b83
AfsarHossain <afsar291722@gmail.com>
2022-01-22 23:47:03 +0600

    Error Type Added

---
+++ Code/30. Error/1. Error Type/main.py
@@ -0,0 +1,15 @@
+# TODO: File Not Found
+# with open("data.txt") as file:
+#     file.read()
+
+# TODO: Key Error
+# dictionary = {"key": "value"}
+# value = dictionary["sadfk"]
+
+# TODO: Index Error
+# fruits_list = ["Apple", "Banana", "Pear"]
+# fruit = fruits_list[3]
+
+# TODO: Type Error
+# text = "abc"
+# print(text + 3)
