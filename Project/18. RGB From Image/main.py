db1d3837b631ca98f984463c6e3df743df8899c8
AfsarHossain <afsar291722@gmail.com>
2022-01-30 11:06:55 +0600

    RGB From Image Project Added

---
+++ Project/18. RGB From Image/main.py
@@ -0,0 +1,48 @@
+import colorgram
+import turtle as turtle_model
+import random
+
+turtle_model.colormode(255)
+timmy = turtle_model.Turtle()
+my_screen = turtle_model.Screen()
+
+# rgb_colors = []
+
+
+# colors = colorgram.extract("images.jpg", 30)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+
+# print(rgb_colors)
+
+color_list = [(201, 164, 112), (239, 246, 241), (152, 75, 50), (221, 201, 138), (57, 95, 126), (170, 152, 44),
+              (138, 31, 20), (135, 163, 183), (196, 94, 75), (49, 121, 88), (143, 177, 149), (95, 75, 77), (76, 39, 32),
+              (164, 146, 157), (16, 98, 71), (232, 176, 165), (54, 46, 48), (32, 61, 76), (22, 83, 89), (182, 204, 176),
+              (141, 22, 25), (86, 147, 127), (45, 66, 85), (8, 68, 53), (177, 94, 97), (222, 177, 182), (109, 128, 151)]
+
+timmy.speed("fastest")
+timmy.penup()
+timmy.hideturtle()
+timmy.setheading(225)
+timmy.forward(300)
+timmy.setheading(0)
+
+number_of_dots = 100
+
+
+for dot_count in range(1, number_of_dots+1):
+    timmy.dot(20, random.choice(color_list))
+    timmy.forward(50)
+
+    if dot_count % 10 == 0:
+        timmy.setheading(90)
+        timmy.forward(50)
+        timmy.setheading(180)
+        timmy.forward(500)
+        timmy.setheading(0)
+
+my_screen.exitonclick()
