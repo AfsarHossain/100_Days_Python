4fb9afdc437cf0b97566fc65cab6dced56bf48e1
AfsarHossain <afsar291722@gmail.com>
2022-02-02 00:07:30 +0600

    Turtle Crossing Start Project Added

---
+++ Project/23. Turtle Crossing Start/main.py
@@ -0,0 +1,41 @@
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from score import Score
+import time
+
+screen = Screen()
+
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+score = Score()
+
+game_is_on = True
+
+screen.listen()
+screen.onkey(player.goto_up, "Up")
+screen.onkey(player.goto_down, "Down")
+
+while game_is_on:
+    time.sleep(.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            score.game_over()
+
+    # Detect successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        score.increase_level()
+
+screen.exitonclick()
